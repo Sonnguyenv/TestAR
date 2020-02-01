831e5449f2f652057b4bae8c84a7a1fbf1fefe2f
Sơn Nguyễn <sonnguyenv.mta@gmail.com>
2020-02-01 23:50:14 +0700

    create Project

---
+++ TestAR/TestAR/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestAR
+//
+//  Created by Sơn Nguyễn on 2/1/20.
+//  Copyright © 2020 Sơn Nguyễn. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
