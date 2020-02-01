0d097187bd73b3451345919eb521cf1b91f53ba4
Sơn Nguyễn <sonnguyenv.mta@gmail.com>
2020-02-02 00:02:38 +0700

    fix UI

---
+++ TestAR/TestAR/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  TestAR
+//
+//  Created by Sơn Nguyễn on 2/1/20.
+//  Copyright © 2020 Sơn Nguyễn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
