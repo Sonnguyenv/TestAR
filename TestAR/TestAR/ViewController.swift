cee0cab82f5a912c5525fa7a832d4ce3d3af622f
Sơn Nguyễn <sonnguyenv.mta@gmail.com>
2020-02-05 23:59:20 +0700

    add model fish

--- TestAR/TestAR/ViewController.swift
+++ TestAR/TestAR/ViewController.swift
@@ -7,11 +7,49 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        addBox()
+        addTapGestureToSceneView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneView.session.pause()
+    }
+    
+    func addBox() {
+        let scene = SCNScene()
+        let fishScene = SCNScene(named: "fishModel.dae")
+        guard let fishNode = fishScene?.rootNode.childNode(withName: "fishModel", recursively: true) else {
+            fatalError("fishModel is not found")
+        }
+        fishNode.position = SCNVector3(0, 0, -1.0)
+        scene.rootNode.addChildNode(fishNode)
+        sceneView.scene = scene
+    }
+    
+    func addTapGestureToSceneView() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.didTap(withGestureRecognizer:)))
+        sceneView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc func didTap(withGestureRecognizer recognizer: UIGestureRecognizer) {
+        let tapLocation = recognizer.location(in: sceneView)
+        let hitTestResults = sceneView.hitTest(tapLocation)
+        guard let node = hitTestResults.first?.node else { return }
+        node.removeFromParentNode()
     }
 }
